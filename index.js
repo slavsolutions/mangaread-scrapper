2e1f58974d3d1b7171e8be964252a32d55d498dc
slavsolutions <tuszkowskislawomir@gmail.com>
2025-11-29 21:44:02 +0100

    progress

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs-extra');
-const readline = require('readline')
+const readline = require('readline');
 
 const chapterLinks = [];
 const downloadImages = require('./components/imageDownloader');
 const createPdfFromImages = require('./components/pdfCreator');
 const getChapterListFromUrl = require('./components/getChapterListFromUrl');
 const getImagesUrlsFromChapterUrl = require('./components/getImagesUrlsFromChapterUrl');
-
-const url = 'https://www.mangaread.org/manga/solo-leveling-manhwa/';
-
+const genres = []
+const url = 'https://www.mangaread.org';
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+async function listGenres (){
+  const helper = [];
+  const {data: html} = await axios.get(url);
+  const $ = cheerio.load(html);
+  const genresFound = $('.menu-item-object-wp-manga-genre');
+  console.log('Ilosc gatunkow: ', genresFound.length)
+  genresFound.each((i,element) => {
+    const anchor = $(element).find('a');
+    //console.log(anchor.text().trim());
+    //console.log(anchor.attr('href'));
+    genres.push([anchor.text().trim(),anchor.attr('href')]) 
+  });
+}
 
-rl.question('Elo, sciagac?',(answer) =>{
-  if(answer === 'yes'){
-    mainHandler(url)
-  }
-})
+// Po załadowaniu danych dynamicznie ustawiamy elementy listy
+async function main() {
+  await listGenres(); // assume this fills global genres: Array of [name, link]
+  const names = genres.map(i => i[0]);
+  genres.map((i,x) =>{
+    console.log(x+1, i[0])
+    
+  })
+  //console.log(genres[0])
+  // /n
+}
+main();
+
 
 async function mainHandler (url){
   const parts = url.split('/').filter(Boolean); // usuwa puste elementy (np. ostatni "/")
